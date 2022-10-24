7ae0cbdb9d76f7ff5ff448ddf6615cca59d79e36
jiashiwen <jiashiwen@ip-10-253-48-130.ap-east-1.compute.internal>
2022-10-24 12:13:06 +0800

    change file name

--- src/cmd/mod.rs
+++ src/cmd/mod.rs
@@ -2,14 +2,14 @@ mod cmdconfig;
 mod cmdloop;
 mod cmdmultilevel;
 mod cmdtask;
-pub mod cmdusediflogger;
+pub mod cmdusedifflogger;
 mod requestsample;
 mod rootcmd;
 
 pub use cmdconfig::new_config_cmd;
 pub use cmdmultilevel::new_multi_cmd;
 pub use cmdtask::new_task_cmd;
-pub use cmdusediflogger::new_use_log_cmd;
+pub use cmdusedifflogger::new_use_log_cmd;
 pub use requestsample::get_baidu_cmd;
 pub use rootcmd::get_command_completer;
 pub use rootcmd::run_app;
